2c1860af84020ec301402a8cba35aa269c4dc986
swo <swo@alum.mit.edu>
2017-04-25 20:57:36 -0400

    cleaning up

--- nw.c
+++ nw.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // #include "chi2.h"
-#include <string.h>
 
 #define DELETE_VALUE -1
 #define INSERT_VALUE 1
@@ -11,8 +11,6 @@
 #define MAX2(x,y) ((x) >= (y) ? (x) : (y))
 #define MIN2(x,y) ((x) <= (y) ? (x) : (y))
 #define MAX3(x,y,z) ((x) >= (y) && (x) >= (z) ? (x) : MAX2(y,z))
-#define MIN3(x,y,z) ((x) <= (y) && (x) <= (z) ? (x) : MIN2(y,z))
-#define MAX4(w,x,y,z) MAX2(MAX3(w, x, y), z)
 
 void print_matrix(int **x, int r, int c) {
     int i;
@@ -26,16 +24,6 @@ void print_matrix(int **x, int r, int c) {
     }
 }
 
-/* char *empty_string(size_t n) { */
-/*     char s[n + 1]; */
-/*     int i; */
-/*     for (i = 0; i < n; i++) { */
-/*         s[i] = '.'; */
-/*     } */
-/*     s[n] = '\0'; */
-/*     return s; */
-/* } */
-
 int nw(char *s1, char *s2) {
     int l1 = strlen(s1);
     int l2 = strlen(s2);
@@ -46,41 +34,50 @@ int nw(char *s1, char *s2) {
     int insert;
     int best;
 
-    int match_score = 1;
-    int mismatch_score = -1;
-    int gap_score = -1;
+    const int match_score = 1;
+    const int mismatch_score = -1;
+    const int gap_score = -1;
 
     int nmm = 0;
 
-    int *m[l1 + 1];
-    for (i = 0; i <= l1; i++)
+    int *score[l1 + 1];
+    for (i = 0; i <= l1; i++) {
         //swo > check that malloc actually allocates
-        m[i] = (int *)malloc((l2 + 1)* sizeof(int));
+        score[i] = (int *)malloc((l2 + 1) * sizeof(int));
+        if (score[i] == NULL) {
+            printf("Memory allocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 
     int *o[l1];
-    for (i = 0; i < l1; i++)
+    for (i = 0; i < l1; i++) {
         o[i] = (int *)malloc(l2 * sizeof(int));
+        if (o[i] == NULL) {
+            printf("Memory allocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 
     for (i = 0; i <= l1; i++)
-        m[i][0] = i * gap_score;
+        score[i][0] = i * gap_score;
 
     for (i = 0; i <= l2; i++)
-        m[0][i] = i * gap_score;
+        score[0][i] = i * gap_score;
 
     for (i = 1; i <= l1; i++) {
         for (j = 1; j <= l2; j++) {
-            //printf("i=%i j=%i\n", i, j);
             if (s1[i - 1] == s2[j - 1]) {
-                match = m[i - 1][j - 1] + match_score;
+                match = score[i - 1][j - 1] + match_score;
             } else {
-                match = m[i - 1][j - 1] + mismatch_score;
+                match = score[i - 1][j - 1] + mismatch_score;
             }
 
-            delete = m[i - 1][j] + gap_score;
-            insert = m[i][j - 1] + gap_score;
+            delete = score[i - 1][j] + gap_score;
+            insert = score[i][j - 1] + gap_score;
 
             best = MAX3(match, delete, insert);
-            m[i][j] = best;
+            score[i][j] = best;
 
             if (best == match) {
                 o[i - 1][j - 1] = MATCH_VALUE;
@@ -95,8 +92,6 @@ int nw(char *s1, char *s2) {
             }
         }
     }
-
-    /* print_matrix(m, l1 + 1, l2 + 1); */
 
     char a1[l1 + l2 + 1];
     char a2[l2 + l2 + 1];
@@ -167,7 +162,7 @@ int nw(char *s1, char *s2) {
     }
 
     for (i = 0; i <= l1; i++)
-        free(m[i]);
+        free(score[i]);
 
     for (i = 0; i < l1; i++)
         free(o[i]);
